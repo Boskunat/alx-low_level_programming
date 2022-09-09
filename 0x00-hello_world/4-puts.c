b8ff386cdfa3432488310ea5e629d08f9ae85858
Mohamed Foday Kunateh <kunatehfm@gmail.com>
2022-09-08 18:48:20 -0700

    C programming language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
-#include<stdio.h>
-
+#include <stdio.h>
 /**
  * printing C
  *
@@ -7,5 +6,6 @@
  */
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle"):
+	return (0)
 }
