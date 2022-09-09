90da7342d996d5e40263f5918e729b0463e525d5
Mohamed Foday Kunateh <kunatehfm@gmail.com>
2022-09-08 18:11:36 -0700

    C programming language

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * printing C
+ *
+ * Return: Always 0 (Successfully)
+ */
+int main(void)
+{
+	put("\"Programming is like building a multilingual puzzle");
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - Prints the size of many var tpyes
+ *
+ * Return: Always 0 (Successfully)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+	printf("size of char %d byte(s)\n", size of (a);
+			printf("size of int %d byte(s)\n", size of (b);
+				printf("size of long int %d byte(s)\n", size of (c);
+				       printf("size of long long int %d byte(s)\n", size of (d);
+				printf("size of float %d byte(s)\n", size of (e);
+			 return (0);
+			 }		 
