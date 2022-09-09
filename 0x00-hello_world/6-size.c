552e31e33951c0da09ea4fa85e009c5792235bf9
Mohamed Foday Kunateh <kunatehfm@gmail.com>
2022-09-08 18:32:34 -0700

    C programming language

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,9 +13,9 @@ int main(void)
 	long long int d;
 	float e;
 	printf("size of char %d byte(s)\n", size of (a);
-			printf("size of int %d byte(s)\n", size of (b);
-				printf("size of long int %d byte(s)\n", size of (c);
-				       printf("size of long long int %d byte(s)\n", size of (d);
-				printf("size of float %d byte(s)\n", size of (e);
-			 return (0);
-			 }		 
+	printf("size of int %d byte(s)\n", size of (b);
+	printf("size of long int %d byte(s)\n", size of (c);
+	printf("size of long long int %d byte(s)\n", size of (d);
+	printf("size of float %d byte(s)\n", size of (e);
+	return (0);
+}		 
