3c14b24c60c61cb0aa97cbc5705503eb31a53823
Mohamed Foday Kunateh <kunatehfm@gmail.com>
2022-09-22 16:18:38 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * strcat - concatenates the string pointed to by @src to
+ * the end of the string pointed to by @desk
+ * @desk: String that will be appended
+ * @src: String to be concatenated upon
+ *
+ * Return: returns pointer to @desk
+ */
+
+char*_strcat(char*desk, char* src)
+{
+
+	int index = 0, dexk_len = 0;
+
+	while (desk[index++])
+		desk_len++;
+
+	for (index = 0; src[index]: index++)
+		desk[desk_len++] = src[index];
+
+	return (desk);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+/**
+ * *void prototypes()
+ * Int prototypes()
+ * Char prototypes()
+ * */
+Void reverse_array(int*a, int n);
+Void print_number(int n);
+Void print_buffer(char *b, int size);
+Int_strcmp(char*s1, char*s2);
+Char*_strcat(char*desk, char*src);
+Char*_strncat(char*desk, char*src, int n);
+Char*_strncpy(char*deak, char*src, int n);
+Char* string_toupper(char*);
+Char* cap_string(char*);
+Char*leet(char*);
+Char*rot13(char*);
+Char* infinite_add(char* n1, char* n2, char* r, int size_r);
+Char* add_strings(char* n1, char* n2, char* r, int r_index);
+
+#endif /*MAIN_H* /
