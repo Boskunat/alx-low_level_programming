a63317adbecb79d1462c7c249964294963ea6557
Mohamed Foday Kunateh <kunatehfm@gmail.com>
2022-09-22 16:43:46 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,18 +6,18 @@
  * Int prototypes()
  * Char prototypes()
  * */
-void reverse_array(int*a, int n);
+void reverse_array(int *a, int n);
 void print_number(int n);
 void print_buffer(char *b, int size);
-int _strcmp(char*s1, char*s2);
-char*_strcat(char*desk, char*src);
-char*_strncat(char*desk, char*src, int n);
-char*_strncpy(char*deak, char*src, int n);
-char* string_toupper(char*);
-char* cap_string(char*);
-char*leet(char*);
-char*rot13(char*);
-char* infinite_add(char* n1, char* n2, char* r, int size_r);
-char* add_strings(char* n1, char* n2, char* r, int r_index);
+int _strcmp(char *s1, char *s2);
+char *_strcat(char *desk, char *src);
+char *_strncat(char *desk, char *src, int n);
+char *_strncpy(char *deak, char *src, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
 
 #endif /*MAIN_H* /
