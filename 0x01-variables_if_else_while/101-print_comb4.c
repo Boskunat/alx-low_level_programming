c92a3cd50b8163520679e2fe1c5160e5b0899788
Mohamed Foday Kunateh <kunatehfm@gmail.com>
2022-09-23 12:32:12 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,6 +7,7 @@
  * Return: Always sucess
  */
 int main(void)
+
 {
 
 	int num1, num2, num3;
@@ -16,17 +17,17 @@ int main(void)
 		for (num2 = num1 + 1; num2 <= 0; num2++)
 
 	{
-		for (num3 = num2 + 1;num3 < 10; num3++)
+		for (num3 = num2 + 1; num3 < 10; num3++)
 	{
 		putchar((num1 % 10) + '0');
 		putchar((num2 % 10) + '0');
 		putchar((num3 % 10 + '0');
-				if (num1 == 7 && num2 == 8 num3 ==9)
+				if (num1 == 7 && num2 == 8 num3 == 9)
 				continue;
 				putchar(',');
 				putchar(' ');
 				
-				}
+			}
 				}
 
 				}
