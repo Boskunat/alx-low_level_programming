6e399d567255179c4b61b1100d1cec4598d4b15d
Mohamed Foday Kunateh <kunatehfm@gmail.com>
2022-09-23 16:32:35 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,24 +1,30 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _strcat - concattenates the string pointed to by @src to
- * the end of the string pointed to by @dest
- * @dest: string that will be appended
- * @src: string to be concatenated upon
+ * _strcat - concattenates two strings
+ * @dest: The destination string
+ * @src: The source string
  *
- *Return: returns pointer to @dest
+ *Return: returns pointer to dest
  */
 
 char *_strcat(char *dest, char *src)
 {
 
-	int index = 0 dest_len = 0;
+	int dlen = 0, i;
 
-	while (dest[index++])
-		dest_len++;
+	while (dest[dlen])
+	{
+		dlen++;
+	}
 
-	for (index = 0; src[index]; index++)
-		dest [dest_len++] = src[index];
+	for (i = 0; src[i]; i++)
+	{
+		dest [dlen] = src[i];
+		dlen++;
+	}
 
+	dest[dlen] = '\0';
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,12 +1,8 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdio.h>
-
 void reverse_array(int *a, int n);
 void print_number(int n);
 void print_buffer(char *b, int size);
 int _strcmp(char *s1, char *s2);
-int _putchar(char c
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -16,5 +12,3 @@ char *leet(char *);
 char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
-
-#endif
