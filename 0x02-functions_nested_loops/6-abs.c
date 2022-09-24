b10a38d7ce7331dd635393f68c33eb7eed16c2f4
Mohamed Foday Kunateh <kunatehfm@gmail.com>
2022-09-24 12:04:17 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute valueof an integer
+ * @ab: input letter
+ * Return: Always success
+ */
+int abs(int ab)
+{
+
+	return (ab * ((ab > 0) - (ab < 0)));
+}
