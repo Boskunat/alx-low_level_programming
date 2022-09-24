996010cbabbb57a2eecd69f70406e606e005b9b3
Mohamed Foday Kunateh <kunatehfm@gmail.com>
2022-09-24 14:48:04 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * jack bauer - function that prints every minute of the day of jack bauer
+ * h = hour, m = minute
+ * Return: Always success
+ */
+void jack_bauer(void)
+{
+
+	int h, m;
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_puchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putcher('\n');
+		}
+	}
+}
