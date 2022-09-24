70d667aa5d6c663f8d4b7b1df91147b0ac15c89d
Mohamed Foday Kunateh <kunatehfm@gmail.com>
2022-09-23 18:26:56 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdliv.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return Aways (success)
+ */
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+	printf('\n');
+
+	return (0);
+}
